8d845717480e20595e94aaa4396b6c4388cc7985
Artem Shinkarov <artyom.shinkaroff@gmail.com>
2016-12-01 18:45:10 +0000

    Put resolution of sac2c dependencies in a sperate file + add comments.

--- cmake/check-sac2c.cmake
+++ cmake/check-sac2c.cmake
@@ -1,4 +1,9 @@
+# This file checks whether we have an operational sac2c compiler.
+
+# SAC2C_EXEC can be passed as an argument to `cmake' call in which
+# case we treat it as a path to the sac2c executable.
 IF (NOT SAC2C_EXEC)
+    # Try to find sac2c and fail if it is not there.
     FIND_PROGRAM (SAC2C_EXEC NAMES "sac2c")
     IF (NOT SAC2C_EXEC)
         MESSAGE (FATAL_ERROR "Could not locate the sac2c binary, exiting...")
